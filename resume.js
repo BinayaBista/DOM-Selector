45a6aebf14fffedff8f0bdc6d63d31270bbf14c7
Binaya Bista (Year Up Professional Resources) <v-bibist@microsoft.com>
2021-02-03 10:21:48 -0800

    DOM Selector HW

---
+++ resume.js
@@ -0,0 +1,18 @@
+var ID = document.getElementById("Objective");
+console.log("ID ", ID);
+
+var Page = document.getElementsByClassName('.Page1');
+console.log("class ", Page);
+
+var Tag = document.getElementsByTagName('Div');
+console.log("Tag ", Tag);
+
+var Query = document.querySelector('.list');
+console.log('QuerySelector ', Query);
+
+var Query1 = document.querySelector('Div');
+console.log('QuerySelector ', Query1);
+
+var QueryAll = document.querySelectorAll('.list');
+console.log("Query All ", QueryAll);
+
